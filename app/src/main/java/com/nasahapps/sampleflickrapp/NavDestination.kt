f5b2bf8c191edb75b5fbd5b4c50a50d5de971a5f
Hakeem Hasan <nasahapps@gmail.com>
2025-04-26 11:49:54 -0500

    Remove unused code

--- app/src/main/java/com/nasahapps/sampleflickrapp/NavDestination.kt
+++ app/src/main/java/com/nasahapps/sampleflickrapp/NavDestination.kt
@@ -1,10 +1,6 @@
 package com.nasahapps.sampleflickrapp
 
-import android.os.Bundle
-import androidx.core.os.BundleCompat
-import androidx.navigation.NavType
 import com.nasahapps.sampleflickrapp.api.FlickrPhoto
-import kotlinx.serialization.json.Json
 
 sealed class NavDestination {
     object Photos : NavDestination() {
@@ -16,23 +12,5 @@ sealed class NavDestination {
             val argPhoto = "photo"
             val route = "photo/{${argPhoto}}"
         }
-    }
-}
-
-val FlickrPhotoType = object : NavType<FlickrPhoto>(false) {
-    override fun get(bundle: Bundle, key: String): FlickrPhoto? {
-        return BundleCompat.getParcelable(bundle, key, FlickrPhoto::class.java)
-    }
-
-    override fun parseValue(value: String): FlickrPhoto {
-        return Json.decodeFromString(value)
-    }
-
-    override fun serializeAsValue(value: FlickrPhoto): String {
-        return Json.encodeToString(value)
-    }
-
-    override fun put(bundle: Bundle, key: String, value: FlickrPhoto) {
-        bundle.putParcelable(key, value)
     }
 }
